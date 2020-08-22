2e5cb791417d74a95563b69614860bbe3e672458
Adarshsathyan <adarshsathyan2000@gmail.com>
2020-08-22 20:41:06 +0530

    Created MOdels for databases

--- crud/core/models.py
+++ crud/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=250)
+    address = models.CharField(max_length=250)
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=200)
+    pincode = models.IntegerField(max_length=30)
