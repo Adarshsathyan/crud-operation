f550948d68b5746b48f70d6664fc7810ec791542
Adarshsathyan <adarshsathyan2000@gmail.com>
2020-08-24 09:37:57 +0530

    Connected upto details page all clear

--- crud/core/views.py
+++ crud/core/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .forms import StudentForm
 from .models import Student
@@ -8,10 +9,14 @@ def index(request):
         if form.is_valid():
             post = form.save()
             post.save()
-            return redirect('index')
+            return redirect('submit')
+
     else:
         form = StudentForm()
     return render(request,'core/reg_form.html',{'form':form})
 def details(request):
     details = Student.objects.all()
     return render(request,'core/reg_details.html',{'details':details})
+
+def submit(request):
+    return render(request,'core/submit.html')
